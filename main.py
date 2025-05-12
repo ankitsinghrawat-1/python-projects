6119915bf393b9cd49fc2e0f92a4a2073dca0bd6
ankitsinghrawat-1 <ankitrawat54328@gmail.com>
2025-05-13 01:19:54 +0530

    README.md

---
+++ main.py
@@ -0,0 +1,20 @@
+# a = (input("enter names:"))
+# print(a) 
+# print(type(a))
+# b = a.split(",")
+# print(b)
+
+# l = [1, 5, 45, 62, "ankit","rawat", "mayank"]
+# print(l)
+# m=l
+# print(m)
+# m[0]= "pushpa"
+# print(m)
+# print(l)
+l = [1, 5, 45, 62, "ankit","rawat", "mayank"]
+print(l)
+m=l.copy()
+print(m)
+m[0]= "pushpa"
+print(m)
+print(l)
