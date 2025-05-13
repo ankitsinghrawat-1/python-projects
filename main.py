4de4eab5d253250922589e5a8cea78b7fe40cf38
ankitsinghrawat-1 <ankitrawat54328@gmail.com>
2025-05-13 19:05:46 +0530

    main.py

--- main.py
+++ main.py
@@ -11,10 +11,26 @@
 # m[0]= "pushpa"
 # print(m)
 # print(l)
-l = [1, 5, 45, 62, "ankit","rawat", "mayank"]
+# l = [1, 5, 45, 62, "ankit","rawat", "mayank"]
+# print(l)
+# m=l.copy()
+# print(m)
+# m[0]= "pushpa"
+# print(m)
+# print(l)
+
+l = [1, 5, 45, 65, 85, 23, "ankit", 'karan']
+l.append(20)
 print(l)
-m=l.copy()
-print(m)
-m[0]= "pushpa"
-print(m)
+# l.sort()
+# print(l)
+# l.sort(reverse=True)
+print(l)
+l.reverse()
+print(l)
+l.remove(45)
+print(l)
+l.pop()
+print(l)
+l.pop(1)
 print(l)
