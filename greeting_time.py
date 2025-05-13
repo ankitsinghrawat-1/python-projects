361a66e786f00e97f5b02abaa4153b0c332058fb
ankitsinghrawat-1 <ankitrawat54328@gmail.com>
2025-05-14 00:10:40 +0530

    greeting_time

---
+++ greeting_time.py
@@ -0,0 +1,16 @@
+import time
+t = time.strftime("%H:%M:%S")
+hour = int(time.strftime("%H"))
+print(t, "this is the current time")
+ask = input("do you want to enter the time manually (y/n):")
+if ask == "y" or ask == "Y":
+    hour = int(input("enter the hour:"))
+    if hour <0 or hour > 23:
+        print("invalid hour")
+        exit()
+if hour >=0 and hour <12:
+    print("Good Morning Sir")
+elif hour >=12 and hour <17:
+    print("Good Afternoon Sir")
+elif hour >=17 and hour <23:
+    print("Good Night Sir")        
