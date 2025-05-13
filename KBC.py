07fd027facf756c8d73ebbbb9e9bca323dd59bc0
ankitsinghrawat-1 <ankitrawat54328@gmail.com>
2025-05-14 01:42:06 +0530

    questions and options

---
+++ KBC.py
@@ -0,0 +1,14 @@
+questions = ["1. What is the capital of Australia?",
+             "2. Which planet is known as the Red Planet?",
+             "What is the Chemcical Formula for Water?",
+             "4. Which color is associated with Peace?",
+             "5. Which is the largest Ocean of Earth?"]
+options = ['''A Melbourne    B Sydney
+C Canberra     D Perth ''','''A Earth    B Saturn
+C Mars     D Jupiter ''','''A H2O2    B H20
+C CO2     D H2''','''A RED     B BLUE
+C WHITE   D GREEN''','''A Atlantic Ocean     B Indian Ocean
+C Pacific Ocean      D Arctic Ocean''']
+for i in range(len(questions)):
+    print(questions[i])
+    print(options[i])
