9bb84de5ded8c799ea4bc95cd497a6814c48c101
ankitsinghrawat-1 <ankitrawat54328@gmail.com>
2025-05-14 11:23:53 +0530

    code

--- KBC.py
+++ KBC.py
@@ -2,13 +2,25 @@ questions = ["1. What is the capital of Australia?",
              "2. Which planet is known as the Red Planet?",
              "What is the Chemcical Formula for Water?",
              "4. Which color is associated with Peace?",
-             "5. Which is the largest Ocean of Earth?"]
+             "5. Which is the largest Ocean of Earth?",
+             "6. Which Scientist is reputed as a 'THIEF'?",
+             "7. Which famous painter is known for his 'Mona Lisa'?",
+             "8. who was the first Indian to win a Oscar?",
+             "9. Which of these is a Professional Football League?",
+             "10. If ice tuns into vapor, what is the process called?",
+             "11. In total how many ways are there that can a batsman get out?"]
 options = ['''A Melbourne    B Sydney
 C Canberra     D Perth ''','''A Earth    B Saturn
 C Mars     D Jupiter ''','''A H2O2    B H20
 C CO2     D H2''','''A RED     B BLUE
 C WHITE   D GREEN''','''A Atlantic Ocean     B Indian Ocean
-C Pacific Ocean      D Arctic Ocean''']
+C Pacific Ocean      D Arctic Ocean''', '''A Einstein    B Newton
+C Tesla       D Edison''', '''A Picasso     B Van Gogh
+C Da Vinci    D Monet''','''A A.R. Rahman     B Gulzar
+C Kamal Hassan    D Lata Mangeshkar''','''A NBA     B NFL
+C ISL     D MLS''','''A Evaporation    B Condensation
+C Sublimation    D Freezing''','''A 5     B 10
+C 6     D 3''']
 for i in range(len(questions)):
     print(questions[i])
     print(options[i])
