29763bbec30731b5193882abdd530f539c0d5780
ankitsinghrawat-1 <ankitrawat54328@gmail.com>
2025-05-17 20:51:47 +0530

    wordstonumber

---
+++ wordtonumber.py
@@ -0,0 +1,6 @@
+from word2number import w2n
+def word_to_number():
+    words = input("Enter a number in words: ")
+    number = w2n.word_to_num(words)
+    print(f"The number is: {number}")   
+word_to_number()    
