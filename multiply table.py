32d88c0be3915c5291ff4b94d582c31f91377b72
ankitsinghrawat-1 <ankitrawat54328@gmail.com>
2025-06-15 19:40:53 +0530

    changes

--- multiply table.py
+++ multiply table.py
@@ -1,6 +1,20 @@
-a = int(input('Enter a number: '))
-print(f"your multiplication table of {a} is:")
-x = int(input("starting range for multiply: "))
-y = int(input("Ending range for multiply: "))
-for i in range(x, y + 1):
-    print(f"{a} x {i} = {a * i}")
+
+while True:
+
+    a = (input('Enter a number: '))
+
+    if a == 'quit' or a == 'exit':
+        print("Exiting the multiplication table program.")
+        break
+    if not a.isdigit():
+        print("Invalid input. Please enter a valid number.")
+        continue
+
+    b = int(a)
+
+    x = int(input("starting range for multiply: "))
+    y = int(input("Ending range for multiply: "))
+
+    print(f"your multiplication table of {b} is:")
+    for i in range(x, y + 1):
+        print(f"{b} x {i} = {b * i}")
