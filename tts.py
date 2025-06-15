b88343c7584ac4c2b9c07bf8ea532ef68bf0eaba
ankitsinghrawat-1 <ankitrawat54328@gmail.com>
2025-06-15 19:45:34 +0530

    changes

--- tts.py
+++ tts.py
@@ -1,5 +1,9 @@
 import pyttsx3 as pyt
-text = input("Enter text to convert to speech: ")
-engine = pyt.init()
-engine.say(text)
-engine.runAndWait()
+while True:
+    text = input("Enter text to convert to speech: ")
+    engine = pyt.init()
+    engine.say(text)
+    engine.runAndWait()
+    if text.lower() == 'quit' or text.lower() == 'exit':
+        print("Exiting the TTS program.")
+        break
