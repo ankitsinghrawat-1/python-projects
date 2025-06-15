8e4b62b378483ef17952d8736f50c6ef6ee583a6
ankitsinghrawat-1 <ankitrawat54328@gmail.com>
2025-06-15 20:25:41 +0530

    changes

--- secret language .py
+++ secret language .py
@@ -3,33 +3,24 @@ import random
 
 letters = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z',
      'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
-ran = random.sample(letters, 3)
-joined = ''.join(ran)
 
-   
-ask = input("Do you want to encode or decode? (e/d): ").lower()
-if ask == 'e':
-     print("You chose to encode.")
-     tex= input("Enter your text: ")
-     text = tex.split()
-     print('encoding your text...')
 
-     for words in text:
-          if len(words) >= 3:
-               print(joined + words + joined)
-          elif len(words) <= 2:
-               print(''.join(reversed(words)))
-elif ask == 'd':
-     print("You chose to decode.")
-     tex = input("Enter your text: ")
-     text = tex.split()
-     print('decoding your text...')
+while True:
+     ran = random.sample(letters, 3)
+     # joined = ''.join(ran)
 
-     for words in text:
-          if len(words) >= 3:
-               print(words[3:-3])
 
-          elif len(words) <= 2:
-               print(''.join(reversed(words)), end=' ')
-          else:
-               print('Invalid encoding detected.')
+     ask = input("Do you want to encode or decode or quit the program? (e/d/quit): ").lower()
+
+     if ask == 'e':
+          text = input("Enter the text to encode: ")
+          words = text.split()
+          encoded = []
+          for word in words:
+               if len(word) >=3:
+                    ntext = ''.join(ran) + word[1:] + word[0] + ''.join(ran)
+                    encoded.append(ntext)
+               elif len(word) == 2:
+                    encoded.append(word[::-1])
+          print("Encoded text:", " ".join(encoded))
+                                        
--- tts.py
+++ tts.py
@@ -1,9 +1,9 @@
 import pyttsx3 as pyt
 while True:
     text = input("Enter text to convert to speech: ")
-    engine = pyt.init()
-    engine.say(text)
-    engine.runAndWait()
     if text.lower() == 'quit' or text.lower() == 'exit':
         print("Exiting the TTS program.")
         break
+    engine = pyt.init()
+    engine.say(text)
+    engine.runAndWait()
