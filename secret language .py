02490d608addfbb6152e3e15e9087a13a6ef4cb4
ankitsinghrawat-1 <ankitrawat54328@gmail.com>
2025-06-15 20:44:37 +0530

    changes

--- secret language .py
+++ secret language .py
@@ -12,6 +12,10 @@ while True:
 
      ask = input("Do you want to encode or decode or quit the program? (e/d/quit): ").lower()
 
+
+     if ask == 'quit':
+          print('Exiting the program...')
+          break
      if ask == 'e':
           text = input("Enter the text to encode: ")
           words = text.split()
@@ -20,7 +24,22 @@ while True:
                if len(word) >=3:
                     ntext = ''.join(ran) + word[1:] + word[0] + ''.join(ran)
                     encoded.append(ntext)
-               elif len(word) == 2:
+               elif len(word) <= 2:
                     encoded.append(word[::-1])
           print("Encoded text:", " ".join(encoded))
-                                        
+
+     elif ask == 'd':
+          text = input("Enter the text to decode: ")
+          words = text.split()
+          decoded = []
+          for word in words:
+               if len(word) >= 9:
+                    wtext = word[3:-3]
+                    ntext = wtext[-1] + wtext[:-1]
+                    decoded.append(ntext)
+               elif len(word) <= 2:
+                    decoded.append(word[::-1])
+          print("Decoded text:", " ".join(decoded))   
+
+     else:
+          print("Invalid input. Please try again.")
